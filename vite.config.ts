6ccde4e6fb96ea23c3fbbcb187d5d118c1251982
yaroslav-shafranskyi <shafranskyiyaroslav@gmail.com>
2023-06-08 15:36:04 +0300

    installed react-native-web

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
   plugins: [react()],
 })
